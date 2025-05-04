ecc60d30aaa7c1cf943c1a7b5738113ec4164b73
João Mauricio <joaomauricio.dev@gmail.com>
2025-05-04 14:20:57 -0300

    feat: add method calcularEstatisticasTransacoes

--- src/main/java/com/joaomauricio/desafio_itau/services/EstatisticasService.java
+++ src/main/java/com/joaomauricio/desafio_itau/services/EstatisticasService.java
@@ -1,7 +1,11 @@
 package com.joaomauricio.desafio_itau.services;
 
+import com.joaomauricio.desafio_itau.controller.dtos.EstatisticasResponseDTO;
+import com.joaomauricio.desafio_itau.controller.dtos.TransacaoRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +18,8 @@ public class EstatisticasService {
         this.transacaoService = transacaoService;
     }
 
-    
+    public EstatisticasResponseDTO calcularEstatisticasTransacoes(Integer intervaloBusca){
+        List<TransacaoRequestDTO> transacoes = transacaoService.buscarTransacoes(intervaloBusca);
+    }
+
 }
