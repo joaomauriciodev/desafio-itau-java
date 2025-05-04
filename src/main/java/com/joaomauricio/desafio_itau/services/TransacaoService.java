685bed40e05b5ad73a0a237d1a51611925548387
João Mauricio <joaomauricio.dev@gmail.com>
2025-05-04 14:14:24 -0300

    feat: add method buscarTransacoes

--- src/main/java/com/joaomauricio/desafio_itau/services/TransacaoService.java
+++ src/main/java/com/joaomauricio/desafio_itau/services/TransacaoService.java
@@ -38,4 +38,12 @@ public class TransacaoService {
     public void limparTransacoes(){
         listaTransacoes.clear();
     }
+
+    public List<TransacaoRequestDTO> buscarTransacoes(Integer intervaloBusca){
+        OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervaloBusca);
+
+        return listaTransacoes.stream()
+                .filter(transacao -> transacao.dataHora().isAfter(dataHoraIntervalo))
+                .toList();
+    }
 }
